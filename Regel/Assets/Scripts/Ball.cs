b9493e765f9d7707d6a87c3b4b633acef34fb289
Agustín Upstaris <amarsero@hotmail.com>
2016-04-01 18:07:32 -0300

    New machine

--- Regel/Assets/Scripts/Ball.cs
+++ Regel/Assets/Scripts/Ball.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+    Rigidbody rigid;
+    Transform CannonTrans;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        rigid = GetComponent<Rigidbody>();
+        CannonTrans = GameObject.FindGameObjectWithTag("Cannon").transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () 
+    {
+	    if (Time.time < 23)
+        {
+            rigid.AddExplosionForce(300, new Vector3(0, -1, -1), 5);
+        }
 	}
 }
---
+++ Regel/Assets/Scripts/Cannon001.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon001 : MonoBehaviour {
+
+
+    public Transform transform;
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
